2ac0705c222b563e7e635ec182053e0980b4a9e7
Timo Strunk <Timo.Strunk@gmail.com>
2024-01-18 13:44:42 +0100

    First version of installer helper

---
+++ install_environment_helper.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+function execute {
+    echo "Executing $1"
+}
+
+function targetver {
+    regex='^([a-zA-Z]*-[0-9]*\.[0-9]*)\S*'
+    if [[ $1 =~ $regex ]]
+    then
+        echo ${BASH_REMATCH[1]}
+    fi
+}
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && (pwd -W 2> /dev/null || pwd))
+ENVDIR="$SCRIPT_DIR/releases"
+
+echo "Nanomatch release installer helper."
+echo "List of available environments:"
+echo 
+for env in $(find $ENVDIR -name "*.conda-lock.yml" | sort -r )
+do
+    envname=$(basename $env .conda-lock.yml)
+    target=$(targetver $envname)
+
+    echo " --- $envname ---"
+    echo "  Environment: $envname to be installed to target env: $target"
+    echo "  To install this environment run:"
+    echo "  micromamba create --name=$target -f $env"
+    echo
+done
